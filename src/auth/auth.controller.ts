89534cd094d7de595613be4d06840fe409e1453d
Erwin <antepuesto.erwin@gmail.com>
2025-04-14 15:50:28 +0800

    partial user login

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Post, UseGuards } from '@nestjs/common';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+
+@Controller('auth')
+export class AuthController {
+  
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  login(){}
+  
+}
