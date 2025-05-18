670dfca1500edbd928a4016c865ea9b5453db627
Erwin <antepuesto.erwin@gmail.com>
2025-05-18 16:08:31 +0800

    product image upload

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, UseGuards, Post, Body, Get } from '@nestjs/common';
+import { Controller, UseGuards, Post, Body, Get, UseInterceptors, UploadedFile, ParseFilePipe, MaxFileSizeValidator, FileTypeValidator } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { CreateProductRequest } from './dto/create-product.request';
 import { TokenPayload } from '../auth/token-payload.interface';
 import { CurrentUser } from '../auth/current-user.decorator';
 import { ProductsService } from './products.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
 @Controller('products')
 export class ProductsController {
@@ -16,6 +19,33 @@ export class ProductsController {
     @CurrentUser() user: TokenPayload
   ){
     return this.productService.createProduct(body, user.userId)
+  }
+
+  @Post(':productId/image')
+  @UseGuards(JwtAuthGuard)
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: 'public/products',
+        filename: (req, file, callback) => {
+          callback(null, `${req.params.productId}${extname(file.originalname)}`
+          )
+        }
+      })
+    })
+  )
+  uploadProductImage(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 50000 }), 
+          new FileTypeValidator({ fileType: 'image/jpeg' })
+        ]
+      })
+    )
+    _file: Express.Multer.File,
+  ){
+    
   }
 
   @Get()
